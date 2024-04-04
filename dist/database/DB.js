e7fb4d2f966dd3c00c7370609e4ff3b9e4b1ccca
McRais <mcrais21@gmail.com>
2024-04-04 18:22:58 +0300

    changed gitignore

---
+++ dist/database/DB.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.postsDB = exports.blogsDB = void 0;
+exports.blogsDB = [
+    {
+        id: "1",
+        name: "First blog",
+        description: "First description",
+        websiteUrl: "First-blog.com"
+    }
+], exports.postsDB = [
+    {
+        id: "1234892487",
+        title: "First post",
+        shortDescription: "Short description for the first post",
+        content: "Content",
+        blogId: "1",
+        blogName: "First blog"
+    }
+];
---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const settings_1 = require("./settings");
+const port = process.env.PORT || 3003;
+settings_1.app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const mongoURI = process.env.MONGO_URL || 'mongodb://0.0.0.0:27017';
+console.log(process.env.MONGO_URL);
+//output - mongodb+srv://a:a@ava.epzello.mongodb.net/?retryWrites=true&w=majority
